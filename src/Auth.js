d26dd6e9f007976ae9ee56835f033e24ea8ab416
David Obodo <obododavid5@gmail.com>
2020-04-25 04:42:44 +0100

    refactor handlevalidate input

--- src/Auth.js
+++ src/Auth.js
@@ -140,83 +140,115 @@ const Auth = () => {
 
   }
 
+  const validateFullName = (value) => {
+    const wordCount = value.split(' ').filter(word => word !== '').length;
+
+    if (value.length < 2) {
+      setFullName({ ...fullName, hasError: true, errorMessage: 'Name must not be less than two characters' })
+    } else if (wordCount < 2 && value.length >= 2) {
+      setFullName({ ...fullName, hasError: true, errorMessage: 'Name must include a space and then a second name' })
+    } else {
+      setFullName({ ...fullName, hasError: false, errorMessage: '' })
+    }
+  }
+
+  const validateEmail = (value) => {
+    var isValid = handleCheckEmailValidity(value)
+    if (!isValid) {
+      setEmail({ ...email, hasError: true, errorMessage: 'Please enter a valid email' })
+    } else {
+      setEmail({ ...email, hasError: false, errorMessage: '' })
+    }
+  }
+
+  const validatePhoneNumber = (value) => {
+    if (value.length !== 11) {
+      setPhone({ ...phone, hasError: true, errorMessage: 'Phone number must be 11 characters' })
+    } else if (value[0] !== '0') {
+      setPhone({ ...phone, hasError: true, errorMessage: 'Nigerian Phone number must begin with a zero' })
+    } else {
+      setPhone({ ...phone, hasError: false, errorMessage: '' })
+    }
+  }
+
+  const validatePassword = (value) => {
+    var isValid = handleCheckPasswordValidity(value)
+    if (!isValid || value.length < 6) {
+      setPassword({ ...password, hasError: true, errorMessage: 'Password must contain at least one uppercase character, one number, special character and not shorter than six characters' })
+    } else {
+      setPassword({ ...password, hasError: false, errorMessage: '' })
+    }
+  }
+
+  const validateConfirmPassword = (value) => {
+    if (value !== password.value) {
+      setConfirmPassword({ ...confirmPassword, hasError: true, errorMessage: 'Must match password field' })
+    } else {
+      setConfirmPassword({ ...confirmPassword, hasError: false, errorMessage: '' })
+    }
+  }
+
+  const validateCardNumber = (value) => {
+    if (value.length !== 19) {
+      setCardNumber({ ...cardNumber, hasError: true, errorMessage: "Card Number must be 16 digits" });
+    } else {
+      setCardNumber({ ...cardNumber, hasError: false, errorMessage: "" });
+    }
+  }
+
+  const validateExpiryDate = (value) => {
+    const day = value.slice(0, 2);
+
+    if (value.length !== 5) {
+      setExpiryDate({ ...expiryDate, hasError: true, errorMessage: "enter complete date" });
+    } else if (day < 1 || day > 31) {
+      setExpiryDate({ ...expiryDate, hasError: true, errorMessage: "enter a valid day" });
+    } else {
+      setExpiryDate({ ...expiryDate, hasError: false, errorMessage: "" });
+    }
+  }
+
+  const validatePin = (value) => {
+    if (value.length !== 4) {
+      setPin({ ...pin, hasError: true, errorMessage: "card pin must be 4 digits" });
+    } else {
+      setPin({ ...pin, hasError: false, errorMessage: "" });
+    }
+  }
+
   const handleValidateInput = e => {
     const { name, value } = e.target;
 
     if (name === 'fullName') {
-      const wordCount = value.split(' ').filter(word => word !== '').length;
-
-      if (value.length < 2) {
-        setFullName({ ...fullName, hasError: true, errorMessage: 'Name must not be less than two characters' })
-      } else if (wordCount < 2 && value.length >= 2) {
-        setFullName({ ...fullName, hasError: true, errorMessage: 'Name must include a space and then a second name' })
-      } else {
-        setFullName({ ...fullName, hasError: false, errorMessage: '' })
-      }
+      validateFullName(value)
     }
 
     if (name === 'email') {
-      var isValid = handleCheckEmailValidity(value)
-      if (!isValid) {
-        setEmail({ ...email, hasError: true, errorMessage: 'Please enter a valid email' })
-      } else {
-        setEmail({ ...email, hasError: false, errorMessage: '' })
-      }
+      validateEmail(value)
     }
 
     if (name === 'phone') {
-      if (value.length !== 11) {
-        setPhone({ ...phone, hasError: true, errorMessage: 'Phone number must be 11 characters' })
-      } else if (value[0] !== '0') {
-        setPhone({ ...phone, hasError: true, errorMessage: 'Nigerian Phone number must begin with a zero' })
-      } else {
-        setPhone({ ...phone, hasError: false, errorMessage: '' })
-      }
+      validatePhoneNumber(value)
     }
 
     if (name === 'password') {
-      var isValid = handleCheckPasswordValidity(value)
-      if (!isValid || value.length < 6) {
-        setPassword({ ...password, hasError: true, errorMessage: 'Password must contain at least one uppercase character, one number, special character and not shorter than six characters' })
-      } else {
-        setPassword({ ...password, hasError: false, errorMessage: '' })
-      }
+      validatePassword(value)
     }
 
     if (name === 'confirmPassword') {
-      if (value !== password.value) {
-        setConfirmPassword({ ...confirmPassword, hasError: true, errorMessage: 'Must match password field' })
-      } else {
-        setConfirmPassword({ ...confirmPassword, hasError: false, errorMessage: '' })
-      }
+      validateConfirmPassword(value)
     }
 
     if (name === 'cardNumber') {
-      if (value.length !== 19) {
-        setCardNumber({ ...cardNumber, hasError: true, errorMessage: "Card Number must be 19 digits" });
-      } else {
-        setCardNumber({ ...cardNumber, hasError: false, errorMessage: "" });
-      }
+      validateCardNumber(value)
     }
 
     if (name === 'expiryDate') {
-      const day = value.slice(0, 2);
-
-      if (value.length !== 5) {
-        setExpiryDate({ ...expiryDate, hasError: true, errorMessage: "enter complete date" });
-      } else if (day < 1 || day > 31) {
-        setExpiryDate({ ...expiryDate, hasError: true, errorMessage: "enter a valid day" });
-      } else {
-        setExpiryDate({ ...expiryDate, hasError: false, errorMessage: "" });
-      }
+      validateExpiryDate(value)
     }
 
     if (name === 'pin') {
-      if (value.length !== 4) {
-        setPin({ ...pin, hasError: true, errorMessage: "card pin must be 4 digits" });
-      } else {
-        setPin({ ...pin, hasError: false, errorMessage: "" });
-      }
+      validatePin(value)
     }
   }
 
